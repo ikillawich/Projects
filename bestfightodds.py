446a1409a138458cada5e8f07e796c02c8dede68
Andrew <andrew.ikalowych@gmail.com>
2017-07-20 21:36:18 -0700

    updatchered the scraper

--- bestfightodds.py
+++ bestfightodds.py
@@ -5,29 +5,62 @@ url = "https://www.bestfightodds.com/"
 class URLopener(urllib.request.FancyURLopener):
     version = "Mozilla/5.0"
 
+
 opener = URLopener()
 res = opener.open(url).read()
 
 soup = bs.BeautifulSoup(res, 'lxml')
 
 div = soup.find_all('div', {'class':'table-header'})
-children = div[0].findChildren()
 
 #Returns event and date
 x = 0
-while x < len(div):
+events = []
+while x < len(div)-1:
     children = div[x].findChildren()
+    fight = []
     for i in range(0,2):
-        print(children[i].text)
+        fight.append(children[i].text)
+    events.append(fight)
     x += 1
 
-fighters = soup.find_all('tr', {'class':'even'})
-for n in fighters:
-    x = n.find_all('th', {"scope":"row"})
-    for n in x:
-        print(n.text)
+#Returns fighters in a nested list
+divtag = soup.find_all('div', {'class':'table-scroller'})
+fighters = []
+for i in range(len(div)-1):
+    f = []
+    for j in range(len(divtag[i].find_all('tr', {'class':['even', 'odd']}))):
+        f.append(divtag[i].find_all('tr', {'class':['even', 'odd']})[j].find_all('th', {"scope":"row"})[0].text)
+    fighters.append(f)
+#print(fighters)
 
-for x in range(0,13):
-    n = x*4
-    print(fighters[n].text)
-    print(fighters[n+1].text)
+#returns odds in a nested list
+odds = []
+for i in range(len(div)-1):
+    o = []
+    for j in range(len(divtag[i].find_all('tr', {'class':['even', 'odd']}))):
+        if(len(divtag[i].find_all('tr', {'class':['even', 'odd']})[j].find_all('span', {"class":"tw"})[1].text) > 4):
+            o.append(divtag[i].find_all('tr', {'class':['even', 'odd']})[j].find_all('span', {"class":"tw"})[1].text[0:-1])
+        else:
+            o.append(divtag[i].find_all('tr', {'class':['even', 'odd']})[j].find_all('span', {"class":"tw"})[1].text)
+    odds.append(o)
+#print(odds)
+
+#This is how they would be added to each fight object
+for i in range(len(div)-1):
+    for j in range(0, len(divtag[i].find_all('tr', {'class':['even', 'odd']})), 2):
+        print(events[i][0], events[i][1], fighters[i][j], odds[i][j], fighters[i][j+1], odds[i][j+1])
+              #event name,   event date,    fighterA,      , oddA      , fighterB     ,   oddB
+
+#prints fighters names line by line
+# odds = []
+# for tag in divtag:
+#     name = tag.find_all('tr', {'class':['even', 'odd']})
+#     for n in name:
+#         llamo = n.find_all('span', {"class":"tw"})
+#         for x in range(1,2):
+#             odds.append
+#             print(llamo[x].text)
+
+#prints of the fighters in event x
+#print(len(divtag[x].find_all('tr', {'class':['even', 'odd']})))
